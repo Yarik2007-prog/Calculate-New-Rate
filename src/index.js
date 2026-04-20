758c50a5a6178f0c3abe7d72a05ca1a058cb6d6b
Yarik <urik7k2@gmail.com>
2026-04-20 14:09:15 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * 
+ * @param {number} rateOfWinner 
+ * @param {number} rateOfLoser 
+ * @returns {number}
+ */
+
+function getRating(rateOfWinner, rateOfLoser) {
+    if (
+        rateOfWinner < 0 ||
+        rateOfLoser < 0 ||
+        typeof rateOfWinner !== "number" ||
+        typeof rateOfLoser !== "number"
+    ) return NaN;
+
+    if (rateOfWinner === 0) {
+        return rateOfLoser;
+    }
+
+    if (rateOfLoser === 0) {
+        return rateOfWinner;
+    }
+
+    if ((rateOfWinner - rateOfLoser) >= 0 && (rateOfWinner - rateOfLoser) <= 2) {
+        return rateOfWinner + 2;
+    }
+
+    if ((rateOfWinner - rateOfLoser) > 2 && (rateOfWinner - rateOfLoser) < 20) {
+        return rateOfWinner + 1;
+    }
+
+    if (rateOfWinner < rateOfLoser) {
+        return Math.round(((rateOfLoser - rateOfWinner + 5) / 3) * 10) / 10;
+    }
+
+    return rateOfWinner;
+}
+
+console.log(getRating(28, 2));
